55a9a46872032c5763f32de5709f228ab43a3d10
vedant-2506 <swamidivate2506@gmail.com>
2026-01-09 10:00:34 +0530

    1 Two Sum

---
+++ 1_TwoSum.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> ans(2);
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    ans[0] = i;
+                    ans[1] = j;
+                    return ans;   
+                }
+            }
+        }
+        return ans; 
+    }
+};
+
+int main() {
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+    cout << "Enter elements in array:\n";
+    for (int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    int target;
+    cout << "Enter target value: ";
+    cin >> target;
+
+    Solution sol;
+    vector<int> result = sol.twoSum(nums, target);
+
+    cout << "Indices are: ";
+    cout << result[0] << " " << result[1] << endl;
+
+    return 0;
+}
