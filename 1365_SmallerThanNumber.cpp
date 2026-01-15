ac95a7b42b66b2bce107853a37722c7a6475d630
vedant-2506 <swamidivate2506@gmail.com>
2026-01-15 09:57:41 +0530

    1365. How Many Numbers Are Smaller Than the Current Number

---
+++ 1365_SmallerThanNumber.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> smallerNumbersThanCurrent(vector<int>& nums) {
+
+        int n=nums.size();
+        vector<int>ans(n);
+
+        for(int i=0;i<n;i++){
+            int count=0;
+            for(int j=0;j<n;j++){
+
+                if( j != i  &&   nums[j] < nums[i]  ){
+
+                    count++;
+                }
+            }
+            ans[i]= count;
+        }
+
+        return ans;
+        
+    }
+};
+
+int main(){
+    cout<<"Smaller than the current number"<<endl;
+
+    vector<int>nums={8,1,2,2,3};
+
+    Solution sol;
+    vector<int>result;
+
+    result = sol.smallerNumbersThanCurrent(nums);
+
+    for(int i=0; i<nums.size(); i++){
+        cout<<result[i]<<" " ;
+    }
+
+    return 0;
+
+
+}
