033d795689ee0d44e86c7704c0d9e58481d3195b
vedant-2506 <swamidivate2506@gmail.com>
2026-03-31 10:40:04 +0530

    20. Valid Parentheses

---
+++ 20_ValidParentheses.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<stack>
+
+using namespace std;
+
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char>stk;
+
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == ')' && !stk.empty() && stk.top() == '(') 
+            stk.pop();
+
+            else if(s[i] == '}' && !stk.empty() && stk.top() == '{') 
+            stk.pop();
+
+            else if(s[i] == ']' && !stk.empty() && stk.top() == '[') 
+            stk.pop();
+
+            else {
+                stk.push( s[i] );
+            }
+
+        }
+
+
+        return stk.empty();
+        
+    }
+};
+
+
+int main(){
+    Solution s;
+    string str = "({[]})";
+    cout<<s.isValid(str)<<endl;
+}
