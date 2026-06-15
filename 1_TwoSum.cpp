a74a8b0b60c6555a6f907494d1854caf48878e69
vedant-2506 <swamidivate2506@gmail.com>
2026-06-15 09:14:52 +0530

    1. Two Sum

--- 1_TwoSum.cpp
+++ 1_TwoSum.cpp
@@ -1,26 +1,59 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+
 using namespace std;
 
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
-        vector<int> ans(2);
 
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (nums[i] + nums[j] == target) {
-                    ans[0] = i;
-                    ans[1] = j;
-                    return ans;   
-                }
+       
+    //    //*A two point approach TC = O(n^2)  SC = O(1) 
+    //    //1)take i strt from 0 to n
+    //    //2)take j strt from 1+1 to n 
+    //    //3) check nums[i] + nums[j] == target 
+    //    //4)if yes push in ans array i and j
+    //    //5return ans array 
+    //     int sz = nums.size();
+    //     vector<int>ans(2);
+
+    //     for(int i =0; i<sz; i++){
+    //         for(int j=i+1; j<sz; j++){
+    //             if(nums[i] + nums[j] == target ){
+    //                 ans[0] = i;
+    //                 ans[1] = j;
+    //             }
+    //         }
+    //     }
+    //     return ans;
+
+        //Using Hash table TC = O(n) SC = O(n)
+        //1)Take a loop of i 0 to n 
+        //2) under loop take x = target - nums[i]
+        //3)find x in hash table 
+        //4)if x in hastable ans[0] = i , ans[1] = um.[x]
+        //  else un[nums[i]] = i
+        //5)return ans arry 
+        int sz = nums.size();
+        unordered_map<int , int>un;
+        vector<int>ans(2); 
+
+        for(int i=0; i<sz; i++){
+            int x = 0;
+            x = target - nums[i];
+            if(un.find( x ) != un.end() ){
+                ans[0] = i;
+                ans[1] = un[x];
             }
-        }
-        return ans; 
+            else{
+                un[nums[i]] = i;
+            }
+        } 
+        return ans;
+
     }
 };
-
 int main() {
     int n;
     cout << "Enter size of array: ";
