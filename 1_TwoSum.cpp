56f444a27a95dc174899cb69d752a07e925e032c
vedant-2506 <swamidivate2506@gmail.com>
2026-06-15 09:45:04 +0530

    1. Two Sum

--- 1_TwoSum.cpp
+++ 1_TwoSum.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <unordered_map>
+#include <algorithm>
 
 using namespace std;
 
@@ -28,32 +29,74 @@ public:
     //     }
     //     return ans;
 
-        //Using Hash table TC = O(n) SC = O(n)
-        //1)Take a loop of i 0 to n 
-        //2) under loop take x = target - nums[i]
-        //3)find x in hash table 
-        //4)if x in hastable ans[0] = i , ans[1] = um.[x]
-        //  else un[nums[i]] = i
-        //5)return ans arry 
-        int sz = nums.size();
-        unordered_map<int , int>un;
-        vector<int>ans(2); 
+        // //Using Hash table TC = O(n) SC = O(n)
+        // //1)Take a loop of i 0 to n 
+        // //2) under loop take x = target - nums[i]
+        // //3)find x in hash table 
+        // //4)if x in hastable ans[0] = i , ans[1] = um.[x]
+        // //  else un[nums[i]] = i
+        // //5)return ans arry 
+        // int sz = nums.size();
+        // unordered_map<int , int>un;
+        // vector<int>ans(2); 
 
-        for(int i=0; i<sz; i++){
-            int x = 0;
-            x = target - nums[i];
-            if(un.find( x ) != un.end() ){
-                ans[0] = i;
-                ans[1] = un[x];
+        // for(int i=0; i<sz; i++){
+        //     int x = 0;
+        //     x = target - nums[i];
+        //     if(un.find( x ) != un.end() ){
+        //         ans[0] = i;
+        //         ans[1] = un[x];
+        //     }
+        //     else{
+        //         un[nums[i]] = i;
+        //     }
+        // } 
+        // return ans;
+
+
+
+        //sorting + 2 pointer  TC = O(nlogn) SC = O(n)
+        //1)Store 
+        //2)Use two pointers i = 0 j = n-1
+        //3) sum = nums[i] + nums[j] 
+        //   if sum == target  ans[0] = i ans[1]=j
+        //4)if sum < target  i++  if sum > target j--
+       //5)return ans array 
+
+        int n = nums.size();
+
+        vector<pair<int,int>> arr;
+
+        for(int i = 0; i < n; i++) {
+            arr.push_back({nums[i], i}); // value, original index
+        }
+
+        sort(arr.begin(), arr.end());
+
+        int i = 0;
+        int j = n - 1;
+
+        while(i < j) {
+
+            int sum = arr[i].first + arr[j].first;
+
+            if(sum == target) {
+                return {arr[i].second, arr[j].second};
             }
-            else{
-                un[nums[i]] = i;
+            else if(sum < target) {
+                i++;
             }
-        } 
-        return ans;
+            else {
+                j--;
+            }
+        }
+
+        return {};
 
     }
 };
+
+
 int main() {
     int n;
     cout << "Enter size of array: ";
