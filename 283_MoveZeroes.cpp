322b7cf65f20b95f4bfee51730e362098da8cd0c
vedant-2506 <swamidivate2506@gmail.com>
2026-06-16 17:31:45 +0530

    283. Move Zeroes

--- 283_MoveZeroes.cpp
+++ 283_MoveZeroes.cpp
@@ -4,28 +4,59 @@
 
 using namespace std;
 
-
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
+    // //1st 2 pointer appoch TC=  SC=
+    //   int j=0;
+    //   for(int i=0; i<nums.size();  i++){
+    //     if(nums[i] != 0){
+    //         swap(nums[i] , nums[j]);
+    //         j++;
+    //     }
+    //   }
 
-        int j=0; 
-        for(int i=0; i<nums.size(); i++){
-            if(nums[i] != 0){
-                swap(nums[i] , nums[j]);
-                j++;
-            }
-        }   
+    //2nd count zero aproch 
+    int j=0, count=0;
+
+    for(int i=0; i<nums.size(); i++){
+        if(nums[i] != 0){
+            nums[j++] =nums[i];
+        }
+        else{
+            count++;
+        }
+    }
+
+    while(count--){
+        nums[j++] = 0;
+    }
+       
     }
 };
 
 int main() {
     Solution sol;
-    vector<int> nums = {0, 1, 0, 3, 12};
+
+    int n;
+    cout << "Enter size of array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter elements: ";
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
     sol.moveZeroes(nums);
-    for (int num : nums) {
+
+    cout << "After moving zeroes: ";
+    for(int num : nums) {
         cout << num << " ";
     }
+
     cout << endl;
+
     return 0;
 }
