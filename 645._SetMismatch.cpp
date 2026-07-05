a838b04a1ee350e6e2adddfe00994000b26199bf
vedant-2506 <swamidivate2506@gmail.com>
2026-07-05 13:49:07 +0530

    645. Set Mismatch

--- 645._SetMismatch.cpp
+++ 645._SetMismatch.cpp
@@ -2,6 +2,7 @@
 #include<vector>
 #include<climits>
 #include<algorithm>
+#include<unordered_map>
 
 
 using namespace std;
@@ -10,41 +11,69 @@ class Solution {
 public:
     vector<int> findErrorNums(vector<int>& nums) {
 
-        int sz=nums.size();
-        vector<int>ans(2);
+        // //find dublicate & missing TC=O(n^2) SC=O(1)
+        // int n = nums.size();
+        // int duplicate=-1, missing =-1;
+        // for(int i=1; i<=n; i++){
+        //     int count=0;
+        //     for(int j=0; j<n; j++){
+        //         if(nums[j] ==i)
+        //           count++;
+        //     }
+        //     if(count == 2) duplicate = i;
+        //     if(count == 0) missing = i;
+        // }
+        // return {duplicate , missing};
+       
 
-        sort(nums.begin(), nums.end());
-        for(int i=0;i<sz-1;i++){
-
-            if(nums[i] == nums[i+1]){
-                ans[0]= nums[i];
-            }
-
-            int sum = sz*(sz+1)/2;
-            int sum_c=0;
-
-            for(int j=0;j<sz;j++){
-                sum_c += nums[j];
-            }
-
-            ans[1] = sum -(sum_c - ans[0]);
-
+        //using hash map TC=O(n) SC=O(n)
+        unordered_map<int,int>un;
+        vector<int>ans;
+        int n=nums.size();
+        //for finding repeting or dublicate 
+        for(int i=0; i<n; i++){
+            un[nums[i]]++;
         }
+       //dublicate number store in ans 
+       for(auto i : un){
+        if(i.second== 2)
+         ans.push_back(i.first);
+       }
+       //find number how not in array 
+       for(int l=1; l<=n; l++){
+        if( un.find(l) == un.end()){
+           ans.push_back(l);
+           break;
+        }
+       }
+       return ans;
 
-        return ans;
-        
+
     }
 };
 
-int main(){
-
-    cout<<"Missing and Repeating Number"<<endl;
-
-    vector<int>heights={1,2,3,3,5, 6,7};
+int main() {
 
     Solution sol;
-    vector<int>result= sol.findErrorNums(heights);
-    cout<<"["<<result[0]<<" , "<< result[1]<<"]"<<endl;
+
+    int n;
+
+    cout << "Find Duplicate and Missing Number " << endl;
+    cout << "Enter the size of the array: ";
+    cin >> n;
+
+    vector<int> nums(n);
+
+    cout << "Enter " << n << " elements (Numbers should be between 1 and " << n << "):" << endl;
+
+    for(int i = 0; i < n; i++) {
+        cin >> nums[i];
+    }
+
+    vector<int> result = sol.findErrorNums(nums);
+
+    cout << "\nResult:" << endl;
+    cout<<result[0]<<","<<result[1]<<endl;
     
     return 0;
 }
