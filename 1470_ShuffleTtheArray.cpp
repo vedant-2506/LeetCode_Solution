c5e1901c6b4539d2d2923b8b359ae002b4041e7f
vedant-2506 <swamidivate2506@gmail.com>
2026-07-06 15:12:05 +0530

    1470.Shuffle The Array

--- 1470_ShuffleTtheArray.cpp
+++ 1470_ShuffleTtheArray.cpp
@@ -1,47 +1,73 @@
 #include<vector>
 #include<climits>
 #include<iostream>
+
 using namespace std;
-
-
-
 class Solution {
 public:
     vector<int> shuffle(vector<int>& nums, int n) {
         
+        // //using for loop TC=O(n) SC=O(n)
+        // int sz=nums.size();
+        // vector<int>ans(sz);
+        // int ans_i=0;
+        // for(int i=0; i<n ;i++){
+
+        //     ans[ans_i]   = nums[i];
+        //     ans[ans_i+1] = nums[i+n];
+
+        //     ans_i +=2;
+        // }
+        // return ans;
+
+        //using while loop TC=O(n) SC=O(n)
         int sz=nums.size();
-
+        int i=0;
         vector<int>ans(sz);
-
-        
-        int ans_i=0;
-        for(int i=0; i<n ;i++){
-
-            ans[ans_i]   = nums[i];
-            ans[ans_i+1] = nums[i+n];
-
-            ans_i +=2;
+        while(i<n){
+            ans[2*i]  =nums[i];
+            ans[2*i+1]=nums[n+i];
+            i++;
         }
-
         return ans;
+        
     }
 };
 
 
-int main(){
-    int n=3;
-
-    vector<int> nums = {2, 5, 1, 3, 4, 7};
+int main() {
 
     Solution obj;
+
+    int n;
+
+    cout << "Enter the value of n (half of the array size): ";
+    cin >> n;
+
+    vector<int> nums(2 * n);
+
+    cout << "Enter " << 2 * n << " elements in the format:" << endl;
+    cout << "x1 x2 ... xn y1 y2 ... yn" << endl;
+    cout << "Example: 2 5 1 3 4 7" << endl;
+    cout << "\nEnter the elements: ";
+
+    for(int i = 0; i < 2 * n; i++) {
+        cin >> nums[i];
+    }
+
     vector<int> ans = obj.shuffle(nums, n);
-    cout << "Answer array is : ";
-    for (int i = 0; i < ans.size(); i++) {
+
+    cout << "\nOriginal Array : ";
+    for(int i = 0; i < 2 * n; i++) {
+        cout << nums[i] << " ";
+    }
+
+    cout << "\nShuffled Array : ";
+    for(int i = 0; i < ans.size(); i++) {
         cout << ans[i] << " ";
     }
 
+    cout << endl;
 
     return 0;
-
-
 }
