4f0b5f6f010a6c80f71ed77c2f017c920c1a3a65
vedant-2506 <swamidivate2506@gmail.com>
2026-07-09 18:04:46 +0530

    121. Best Time to Buyan Sell Stock

--- 121_BestTimetoBuyanSellStock.cpp
+++ 121_BestTimetoBuyanSellStock.cpp
@@ -7,6 +7,18 @@ using namespace std;
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
+
+        // //Brute Force TC=(n^2) SC=O(1) time  Time Limit Exceeded
+        // int maxProfit=0;
+        // for(int i=0; i<prices.size(); i++){
+        //     for(int j=i+1; j<prices.size(); j++){
+        //         int profit = prices[j] - prices[i];
+        //         maxProfit = max(maxProfit , profit);
+        //     }
+        // }
+        // return maxProfit;
+        
+        //Greedy TC=O(n) SC=O(1)
         int sp=prices[0];
         int profit =0 , maxProfit=0;
         for(int k=1; k<prices.size(); k++){
@@ -18,10 +30,25 @@ public:
     }
 };
 
-int main(){
+int main() {
+
+    int n;
+
+    cout << "Enter the number of days: ";
+    cin >> n;
+
+    vector<int> prices(n);
+
+    cout << "Enter the stock prices: ";
+    for(int i = 0; i < n; i++) {
+        cin >> prices[i];
+    }
+
     Solution s;
-    vector<int> prices = {7,1,5,3,6,4};
-    cout<<s.maxProfit(prices);
-    cout<<endl;
-    return  0;
+
+    int result = s.maxProfit(prices);
+
+    cout << "Maximum Profit: " << result << endl;
+
+    return 0;
 }
