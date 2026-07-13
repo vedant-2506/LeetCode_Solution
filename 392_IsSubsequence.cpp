ed479f4aafb53d4dfbd48def5d360679c1527907
vedant-2506 <swamidivate2506@gmail.com>
2026-07-13 11:18:28 +0530

    392. Is Subsequence

--- 392_IsSubsequence.cpp
+++ 392_IsSubsequence.cpp
@@ -1,44 +1,44 @@
 #include<iostream>
 #include<string>
+
 using namespace std;
 
 class Solution {
 public:
     bool isSubsequence(string s, string t) {
-        int sz1 = s.size() , sz2=t.size() ;
-        int n= sz1;
 
-        int i=0,j=0;
-        while(i<sz1  &&  j<sz2){
-            if(s[i] == t[j]){
-                i++,j++;
-                n--;
-            }
-            else{
-                j++;
-            }
+        //two pointer TC=(m+n) SC=(1)
+        int i=0  , j=0;
+        while(i<s.size() && j<t.size()){
+            if(s[i] == t[j]) i++;
+            j++;
         }
-
-        return (n<=0) ? true : false;
+        return (i == s.size());
         
     }
 };
 
-int main(){
-    string s , t;
+int main() {
 
-    cout<<"Enter two strings to check if first is subsequence of second :"<<endl;
-    cin>>s>>t;
+    string s, t;
+
+    cout << "Enter the first string (subsequence candidate): ";
+    cin >> s;
+
+    cout << "Enter the second string: ";
+    cin >> t;
 
     Solution sol;
 
-    bool result = sol.isSubsequence(s,t);
+    bool result = sol.isSubsequence(s, t);
 
-    if(result){
-        cout<<"Yes, first string is subsequence of second string"<<endl;
+    cout << "\nResult: ";
+
+    if(result) {
+        cout << "\"" << s << "\" is a subsequence of \"" << t << "\"." << endl;
     }
-    else{
-        cout<<"No, first string is not subsequence of second string"<<endl;
+    else {
+        cout << "\"" << s << "\" is NOT a subsequence of \"" << t << "\"." << endl;
     }
 
     return 0;
