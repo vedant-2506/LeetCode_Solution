8a3f60b9cafa9a2864f3ab1248ec09e8a6e1e0c7
vedant-2506 <swamidivate2506@gmail.com>
2026-07-13 10:49:42 +0530

    387. First Unique Character in a String

--- 387_FirstUniqueCharacterInString.cpp
+++ 387_FirstUniqueCharacterInString.cpp
@@ -8,30 +8,58 @@ class Solution {
 public:
     int firstUniqChar(string s) {
 
-        unordered_map<char , int>un;
+        // //nested loop TC=O(n^2) SC=(1) Time Limit Exceeded
+        // for(int i=0; i<s.size(); i++){
+        //     int count =0;
+        //     for(int j=0; j<s.size(); j++){
+        //         if(s[i] == s[j] ) count ++;
+        //     }
+        //     if(count == 1) return i;
+        // }
+        // return -1;
+        
+        // //using hashmap TC=(n) SC=O(n)
+        // unordered_map<char , int>un;
+        // for(char i : s){
+        //     un[i]++;
+        // }
+        // for(int j=0; j<s.size(); j++){
+        //     if(un[ s[j] ] == 1) return j;
+        // }
+        // return -1;
+        
+
+        //frequncy array TC=(n) SC=(1)
+        int ferq[26] = {0};
+
+        for(char ch : s){
+            ferq[ch - 'a']++;
+        }
 
         for(int i=0; i<s.size(); i++){
-            un[s[i]]++;
+            if(ferq[ s[i] - 'a'] == 1) return i;
         }
-
-        for(int j=0; j<s.size(); j++){
-            if(un.find( s[j] ) != un.end() && un[ s[j] ] == 1) return j;
-        }
-
         return -1;
-        
     }
 };
 
+int main() {
 
-int main(){
+    string s;
 
-    string s = "leetcode";
+    cout << "Enter a string: ";
+    cin >> s;
 
     Solution sol;
     int ans = sol.firstUniqChar(s);
 
-    cout<<ans<<endl;
+    if(ans != -1) {
+        cout << "\nIndex of the first unique character: " << ans << endl;
+        cout << "Character: " << s[ans] << endl;
+    }
+    else {
+        cout << "\nNo unique character found in the string." << endl;
+    }
 
     return 0;
 }
