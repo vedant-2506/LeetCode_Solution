471795d7b510e6333dc8886f68a65c899cd1a015
vedant-2506 <swamidivate2506@gmail.com>
2026-07-13 09:53:36 +0530

    344.Reverse String

--- 344_ReverseString.cpp
+++ 344_ReverseString.cpp
@@ -7,30 +7,49 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
 
-        int i=0;
-        int j=s.size()-1;
-
-        while(i < j){
-            swap(s[i] ,s[j]);
-            i++;
-            j--;
+        //using extra spec TC=O(n) SC=O(n)
+        vector<char>temp;
+        for(int i=s.size()-1; i>=0; i--){
+            temp.push_back( s[i] );
         }
-        
+        s=temp;
+
+        // //inpace + swap() TC=O(n) SC=(1)
+        // int i=0, j=s.size()-1;
+        // while(i<j){
+        //     swap(s[i] , s[j]);
+        //     i++;
+        //     j--;
+        // }
+
     }
-};
+}; 
 
+int main() {
 
-int main(){
+    int n;
 
-    vector<char> s = {'h','e','l','l','o'};
+    cout << "Enter the number of characters: ";
+    cin >> n;
+
+    vector<char> s(n);
+
+    cout << "Enter the characters:\n";
+    for(int i = 0; i < n; i++) {
+        cout << "Character " << i + 1 << ": ";
+        cin >> s[i];
+    }
 
     Solution sol;
     sol.reverseString(s);
 
-    for(char c : s){
-        cout<<c<<" ";
+    cout << "\nReversed characters: ";
+    for(char ch : s) {
+        cout << ch << " ";
     }
-    cout<<endl;
+
+    cout << endl;
 
     return 0;
 }
+
