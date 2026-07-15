b90f3a2a931c3bb78756c8c7ccd085af6549d85b
vedant-2506 <swamidivate2506@gmail.com>
2026-07-15 14:03:43 +0530

    1768. Merge Strings Alternately

--- 1768_MergeStringsAlternately.cpp
+++ 1768_MergeStringsAlternately.cpp
@@ -1,50 +1,62 @@
 #include<iostream>
 #include<string>
+
 using namespace std;
+
 class Solution {
 public:
     string mergeAlternately(string word1, string word2) {
         
-        string ans;
-        int sz1 = word1.size();
-        int sz2 = word2.size();
+        // //two pointer using same loop 
+        // // TC=O(m+n) SC=(m+n)
+        // string ans="";
+        // int k=0;
 
-        int i=0,j=0;
-        while(i<sz1 && j<sz2) {
-           ans   += word1[i];
-           ans   += word2[j];
+        // while(k<word1.size() || k<word2.size() ){
+        //     if(k<word1.size() ){
+        //         ans+=word1[k];
+        //     }
+        //     if(k<word2.size()){
+        //         ans+=word2[k];
+        //     }
+        //     k++;
+        // }
 
-            i++,j++;
-        }
+        // return ans;
 
-        if(i<sz1){
-        while(i<sz1){
-            ans += word1[i];
-            i++;
+        //Two pointer using multiple loop 
+        //TC=O(m+n SC=O(m+n)
+        string ans="";
+        int i=0 , j=0;
+        while(i<word1.size() && j<word2.size()){
+            ans+=word1[i++];
+            ans+=word2[j++];
         }
+        while(i<word1.size()){
+            ans+=word1[i++];
         }
-        else{
-        while(j<sz2){
-            ans += word2[j];
-            j++;
+        while(j<word2.size()){
+            ans+=word2[j++];
         }
-        }
-        
-        
         return ans;
+        
     }
 };
 
-
-int main(){
-    string word1 , word2;
-
-    cout<<"Enter two words  to merge them alternately :"<<endl;
-    cin>>word1>>word2;
+int main() {
 
     Solution sol;
+    string word1, word2;
 
-    string result = sol.mergeAlternately(word1,word2);
+    cout << "Enter the first string : ";
+    cin >> word1;
+
+    cout << "Enter the second string: ";
+    cin >> word2;
+
+    string result = sol.mergeAlternately(word1, word2);
+
+    cout << "\nMerged String: " << result << endl;
 
     return 0;
 }
