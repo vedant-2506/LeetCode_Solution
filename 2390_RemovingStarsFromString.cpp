9a7207078915ff50397c951aa7edcc83146e241e
vedant-2506 <swamidivate2506@gmail.com>
2026-07-20 08:52:22 +0530

    2390. Removing Stars From a String

--- 2390_RemovingStarsFromString.cpp
+++ 2390_RemovingStarsFromString.cpp
@@ -7,33 +7,51 @@ using namespace std;
 class Solution {
 public:
     string removeStars(string s) {
-        stack<char>stk;
 
-       for(int i=0;i<s.size();i++){
-        if(s[i] == '*'){
-            stk.pop();
+        // //Stack Simulation TC=(n) SC=O(n)
+        // stack<char>stk;
+        // for(int i=0; i<s.size(); i++){
+        //     if(!stk.empty() && s[i] == '*'){
+        //         stk.pop();
+        //         continue;
+        //     }
+        //     stk.push(s[i]);
+        // }     
+        // string ans="";   
+        // while(!stk.empty()){
+        //     ans+=stk.top();
+        //     stk.pop();
+        // }
+        // reverse(ans.begin() , ans.end());
+        // return ans;
+
+        //In-place Stack Simulation (Two Pointer)
+        //TC=O(n) SC=O(1)
+        int j=0;
+        for(int i=0; i<s.size(); i++){
+            if(s[i] == '*'){
+                j--;
+            }
+            else{
+                s[j] = s[i];
+                j++;
+            }
         }
-        else{
-            stk.push( s[i] );
-        }
-       }
-
-       string ans="";
-       while(!stk.empty()){
-        ans+=stk.top();
-        stk.pop();
-       }
-
-       reverse(ans.begin(),ans.end());
-
-       return ans;
-        
+        return s.substr(0,j);
     }
 };
 
-int main(){
-    Solution s;
-    string str = "leet**cod*e";
-    cout<<s.removeStars(str)<<endl;
+int main() {
+
+    Solution sol;
+    string s;
+
+    cout << "Enter a string (use '*' to represent a backspace): ";
+    cin >> s;
+
+    string result = sol.removeStars(s);
+
+    cout << "\nString after processing: " << result << endl;
+
     return 0;
 }
